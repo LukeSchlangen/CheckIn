bde6bf526f88ab2ede7851a48bbb3c5ec69cddd1
Thomas Rude <tom_rude_08@hotmail.com>
2014-09-28 01:16:09 -0700

    Commit all expecting merge

--- src/com/abamath/checkin/client/AbamathService.java
+++ src/com/abamath/checkin/client/AbamathService.java
@@ -9,7 +9,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 /**
  * The client-side stub for the RPC service.
  */
-@RemoteServiceRelativePath("greet")
+@RemoteServiceRelativePath("abamathServlet")
 public interface AbamathService extends RemoteService {
 	void buttonClick(User user) throws IllegalArgumentException;
 	List<User> getUsers();
