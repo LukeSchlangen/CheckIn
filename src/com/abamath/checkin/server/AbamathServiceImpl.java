01538eaccbb090335f8da988c9e830c0d88399d3
Thomas Rude <tom_rude_08@hotmail.com>
2014-10-12 23:07:16 -0700

    more back end auth changes

--- src/com/abamath/checkin/server/AbamathServiceImpl.java
+++ src/com/abamath/checkin/server/AbamathServiceImpl.java
@@ -14,10 +14,15 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.AttributeValueUpdate;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
 import com.amazonaws.services.dynamodbv2.model.GetItemResult;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
@@ -28,110 +33,194 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
  * The server-side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
-public class AbamathServiceImpl extends RemoteServiceServlet implements AbamathService {
+public class AbamathServiceImpl extends RemoteServiceServlet implements
+		AbamathService {
 	private static AmazonDynamoDB dynamoDB;
 	private final static String MEMBER_TABLE_NAME = "Members";
 	private final static String HISTORY_TABLE_NAME = "check-in-times";
 	private final static String AUTHENTICATION_TABLE_NAME = "Authentication";
 	private final static String END_POINT = "dynamodb.us-west-2.amazonaws.com";
+	private final static String MEMBER_TABLE_PRIMARY_KEY = "Name";
+	private final static String MEMBER_TABLE_RANGE_KEY = "Color";
+	private final static String CHECKIN_TABLE_PRIMARY_KEY = "Time";
+	private final static String CHECKIN_TABLE_RANGE_KEY = "Member";
+	private final static String TABLE_TYPE_STRING = "S";
+	private final static long READ_TP = 1L;
+	private final static long WRITE_TP = 1L;
 	
+
 	public AbamathServiceImpl() throws IOException {
 		setupDB();
 	}
-	
+
 	@Override
 	public void buttonClick(User user) throws IllegalArgumentException {
 		try {
 			Map<String, AttributeValue> key = new HashMap<String, AttributeValue>();
-			key.put("Name", new AttributeValue().withS(user.getName()));			
+			key.put("Name", new AttributeValue().withS(user.getName()));
 			Map<String, AttributeValueUpdate> update = new HashMap<String, AttributeValueUpdate>();
-			update.put("Color", new AttributeValueUpdate().withValue(new AttributeValue().withS(user.getColor())));	
-			update.put("Status", new AttributeValueUpdate().withValue(new AttributeValue().withS(user.getStatus())));
-			if(user.getStatus().equals("Out")) {
-				update.put("Time", new AttributeValueUpdate().withValue(new AttributeValue().withS(user.getTime())));
+			update.put("Color", new AttributeValueUpdate()
+					.withValue(new AttributeValue().withS(user.getColor())));
+			update.put("Status", new AttributeValueUpdate()
+					.withValue(new AttributeValue().withS(user.getStatus())));
+			if (user.getStatus().equals("Out")) {
+				update.put("Time", new AttributeValueUpdate()
+						.withValue(new AttributeValue().withS(user.getTime())));
 			}
 			UpdateItemRequest updateRequest = new UpdateItemRequest()
-					.withTableName(MEMBER_TABLE_NAME)
-					.withKey(key)
+					.withTableName(MEMBER_TABLE_NAME).withKey(key)
 					.withAttributeUpdates(update);
 			dynamoDB.updateItem(updateRequest);
 
 			key = new HashMap<String, AttributeValue>();
-			key.put("Time", new AttributeValue().withS(new Timestamp(new Date().getTime()).toString()));
+			key.put("Time", new AttributeValue().withS(new Timestamp(new Date()
+					.getTime()).toString()));
 			key.put("Member", new AttributeValue().withS(user.getName()));
-			PutItemRequest putRequest = new PutItemRequest()
-				.withTableName(HISTORY_TABLE_NAME)
-				.withItem(key);
+			PutItemRequest putRequest = new PutItemRequest().withTableName(
+					HISTORY_TABLE_NAME).withItem(key);
 			dynamoDB.putItem(putRequest);
-		
+
 		} catch (Exception e) {
 			throw e;
 		}
 	}
-	
+
 	@Override
 	public List<User> getUsers() {
 		List<User> userList = new ArrayList<User>();
-		ScanRequest request = new ScanRequest().withTableName(MEMBER_TABLE_NAME);
+		ScanRequest request = new ScanRequest()
+				.withTableName(MEMBER_TABLE_NAME);
 		ScanResult result = dynamoDB.scan(request);
-			
-		for(Map<String, AttributeValue> map : result.getItems()) {
-			User user = new User();				
+
+		for (Map<String, AttributeValue> map : result.getItems()) {
+			User user = new User();
 			user.setName(map.get("Name").getS());
 			user.setColor(map.get("Color").getS());
 			user.setStatus(map.get("Status").getS());
 			user.setTime(map.get("Time").getS());
 			userList.add(user);
 		}
-					
-		return userList;		
+
+		return userList;
 	}
-	
+
 	@Override
 	public boolean createAccount(String username, String password) {
-		if(username.equals("") || password.equals("")) {
+		if (username.equals("") || password.equals("")) {
 			return false;
 		}
+
+		//create member table for the user
+		ArrayList<KeySchemaElement> ksMember = new ArrayList<KeySchemaElement>();
+		ArrayList<AttributeDefinition> adMember = new ArrayList<AttributeDefinition>();
+
+		//Members - primary key
+		ksMember.add(new KeySchemaElement()
+			.withAttributeName(MEMBER_TABLE_PRIMARY_KEY)
+			.withKeyType(KeyType.HASH));
+		adMember.add(new AttributeDefinition()
+			.withAttributeName(MEMBER_TABLE_PRIMARY_KEY)
+			.withAttributeType(TABLE_TYPE_STRING));
 		
+		//Color - range key
+		ksMember.add(new KeySchemaElement()
+			.withAttributeName(MEMBER_TABLE_RANGE_KEY)
+			.withKeyType(KeyType.RANGE));
+		adMember.add(new AttributeDefinition()
+			.withAttributeName(MEMBER_TABLE_RANGE_KEY)
+			.withAttributeType(TABLE_TYPE_STRING));
+		
+		ProvisionedThroughput ptMember = new ProvisionedThroughput()
+			.withReadCapacityUnits(READ_TP)
+			.withWriteCapacityUnits(WRITE_TP);
+		
+		String membersTableName = username + "_members_" + 1;
+		
+		CreateTableRequest membersTableRequest = new CreateTableRequest()
+			.withTableName(membersTableName)
+			.withKeySchema(ksMember)
+			.withProvisionedThroughput(ptMember)
+			.withAttributeDefinitions(adMember);
+		
+		dynamoDB.createTable(membersTableRequest);
+		
+		//create checkin times table for the user
+		ArrayList<KeySchemaElement> ksCheckin = new ArrayList<KeySchemaElement>();
+		ArrayList<AttributeDefinition> adCheckin = new ArrayList<AttributeDefinition>();
+
+		//Time - primary key
+		ksCheckin.add(new KeySchemaElement()
+			.withAttributeName(CHECKIN_TABLE_PRIMARY_KEY)
+			.withKeyType(KeyType.HASH));
+		adCheckin.add(new AttributeDefinition()
+			.withAttributeName(CHECKIN_TABLE_PRIMARY_KEY)
+			.withAttributeType(TABLE_TYPE_STRING));
+		
+		//Member - range key
+		ksCheckin.add(new KeySchemaElement()
+			.withAttributeName(CHECKIN_TABLE_RANGE_KEY)
+			.withKeyType(KeyType.RANGE));
+		adCheckin.add(new AttributeDefinition()
+			.withAttributeName(CHECKIN_TABLE_RANGE_KEY)
+			.withAttributeType(TABLE_TYPE_STRING));
+		
+		ProvisionedThroughput pt = new ProvisionedThroughput()
+			.withReadCapacityUnits(READ_TP)
+			.withWriteCapacityUnits(WRITE_TP);
+		
+		String checkinTableName = username + "_checkin_" + 1;
+		
+		CreateTableRequest checkinTableRequest = new CreateTableRequest()
+			.withTableName(checkinTableName)
+			.withKeySchema(ksCheckin)
+			.withProvisionedThroughput(pt)
+			.withAttributeDefinitions(adCheckin);
+		
+		dynamoDB.createTable(checkinTableRequest);
+
+		// create the account in the auth table
 		HashMap<String, AttributeValue> key = new HashMap<String, AttributeValue>();
 		key.put("Username", new AttributeValue().withS(username));
 		key.put("Password", new AttributeValue().withS(password));
 		key.put("Salt", new AttributeValue().withS(password));
+		key.put("Tables", new AttributeValue().withN("1"));
 		PutItemRequest putRequest = new PutItemRequest()
 			.withTableName(AUTHENTICATION_TABLE_NAME)
 			.withItem(key);
 		dynamoDB.putItem(putRequest);
-		
+
 		return true;
 	}
-	
+
 	@Override
 	public boolean authenticate(String username, String password) {
-		if(password.isEmpty()) {
+		if (password.isEmpty()) {
 			return false;
 		}
 		Map<String, AttributeValue> key = new HashMap<String, AttributeValue>();
 		key.put("Username", new AttributeValue().withS(username));
 		key.put("Password", new AttributeValue().withS(password));
-		GetItemRequest getRequest = new GetItemRequest().withTableName(AUTHENTICATION_TABLE_NAME).withKey(key);
+		GetItemRequest getRequest = new GetItemRequest().withTableName(
+				AUTHENTICATION_TABLE_NAME).withKey(key);
 		GetItemResult getResult = dynamoDB.getItem(getRequest);
-		if(getResult.getItem() == null) {
-			//username doesn't exist in the db
+		if (getResult.getItem() == null) {
+			// username doesn't exist in the db
 			return false;
-		}
-		else {
-			if(getResult.getItem().get("Password").getS().equals(password)) {
-				//if user exists and passwords match
+		} else {
+			if (getResult.getItem().get("Password").getS().equals(password)) {
+				// if user exists and passwords match
 				return true;
 			}
 		}
 
 		return false;
 	}
-	
+
 	private static void setupDB() throws IOException {
-		
-		AWSCredentials credentials = new PropertiesCredentials(AbamathServiceImpl.class.getResourceAsStream("AWSCredentials.properties"));
+
+		AWSCredentials credentials = new PropertiesCredentials(
+				AbamathServiceImpl.class
+						.getResourceAsStream("AWSCredentials.properties"));
 		dynamoDB = new AmazonDynamoDBClient(credentials);
 		dynamoDB.setEndpoint(END_POINT);
 	}
