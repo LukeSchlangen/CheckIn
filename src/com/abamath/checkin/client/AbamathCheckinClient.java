023cae40396135940fb8723b85f36b19400a938c
akushare <aditkushare@gmail.com>
2014-10-26 18:14:06 -0500

    This is the timer commit

--- src/com/abamath/checkin/client/AbamathCheckinClient.java
+++ src/com/abamath/checkin/client/AbamathCheckinClient.java
@@ -3,11 +3,13 @@ package com.abamath.checkin.client;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import com.abamath.checkin.shared.User;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTMLPanel;
@@ -18,45 +20,45 @@ import com.google.gwt.user.client.ui.Panel;
 public class AbamathCheckinClient implements AbamathClient {
 
 	private AbamathServiceAsync service;
-	
+
 	private static final String[] colors = new String[]{"Yellow","Green","Red", "Blue"};
-	
+
 	private Panel returnPanel;
 	private HTMLPanel outPanel;
 	private HTMLPanel inPanel;
 	private Label outHeader;
 	private Label inHeader;
-	
+
 	public AbamathCheckinClient(AbamathServiceAsync service, AbamathCheckinEntryPoint entryPoint) {
 		this.service = service;
 		setupPanelForRoot();
 	}
-	
+
 	@Override
 	public Panel getPanelForRoot() {
 		History.newItem("home");
 		return returnPanel;
 	}
-	
+
 	@Override
 	public void setupPanelForRoot() {
 		returnPanel = new HorizontalPanel();
 		outPanel = new HTMLPanel("");
 		inPanel = new HTMLPanel("");
-		
+
 		outHeader = new Label("OUT");
 		inHeader = new Label("IN");
-		
+
 		outPanel.add(outHeader);
 		inPanel.add(inHeader);
-		
+
 		showButtons();
 		addCss();
 
-	    returnPanel.add(outPanel);
-	    returnPanel.add(inPanel);
+		returnPanel.add(outPanel);
+		returnPanel.add(inPanel);
 	}
-	
+
 	private void showButtons() {
 		service.getUsers(new AsyncCallback<List<User>>() {
 			@Override
@@ -68,16 +70,19 @@ public class AbamathCheckinClient implements AbamathClient {
 			}
 			@Override
 			public void onSuccess(List<User> result) {
-				
+
+
+
 				for(int i = 0; i < result.size(); i++) {
 					User user = result.get(i);
-					
-					double displayTime = (double)(Integer.parseInt(user.getTime())/6)/10;
-					Button button = new Button("<namelabel>" + user.getName() + "</namelabel><br/>" + displayTime + " Hours");
+
+
+
+					String displayTime =  stringTimeFormatter(user);
+					Button button = new Button("<namelabel>" + user.getName() + "</namelabel><br/>" + displayTime );
 					button.addClickHandler(new MyHandler(user));
-					
 					addButtonCss(button, user);
-					
+
 					if(user.getStatus().equals("Out")) {
 						outPanel.add(button);
 					}
@@ -89,7 +94,33 @@ public class AbamathCheckinClient implements AbamathClient {
 			}			
 		});
 	}
-	
+	private String stringTimeFormatter(User user)
+	{
+
+		String result = "";
+		int hours = ((Integer.parseInt(user.getTime())/60)) ;
+		int minutes = ((Integer.parseInt(user.getTime())%60));
+		String hoursString;
+		String minutesString;
+		if(hours<10)
+		{
+			hoursString = "0" +  hours ;
+		}
+		else
+		{
+			hoursString = hours + "";
+		}
+		if(minutes < 10)
+		{
+			minutesString = "0" + minutes;
+		}
+		else
+		{
+			minutesString = minutes +  "";
+		}
+		result = hoursString + " : " + minutesString + " : " +"00 " + "Hours" ;
+		return result;
+	}
 	@Override
 	public void addCss() {
 		outPanel.addStyleName("panel");
@@ -99,7 +130,7 @@ public class AbamathCheckinClient implements AbamathClient {
 		outHeader.addStyleName("panel-heading");
 		inHeader.addStyleName("panel-heading");		
 	}
-	
+
 	public void addButtonCss(Button button, User user) {
 		button.addStyleName("btn-default");
 		button.addStyleName("col-xs-12");
@@ -107,57 +138,62 @@ public class AbamathCheckinClient implements AbamathClient {
 		button.addStyleName("col-md-4");
 		button.addStyleName("col-lg-3");
 		button.addStyleName("col-xl-2");
-		
-		
+
+
 		if(Integer.parseInt(user.getTime())<1200){
 			switch (user.getColor()) {
-            case "Yellow":  button.addStyleName("yellow");
-                     break;
-            case "Green":  button.addStyleName("green");
-                     break;
-            case "Red":  button.addStyleName("red");
-                     break;
-            case "Blue": button.addStyleName("blue");
-            		break;
-            default:
-            		int random = (int)(Math.random() * 4);
-            		user.setColor(colors[random]);
-            		button.addStyleName(colors[random].toLowerCase());
-                     break;
+			case "Yellow":  button.addStyleName("yellow");
+			break;
+			case "Green":  button.addStyleName("green");
+			break;
+			case "Red":  button.addStyleName("red");
+			break;
+			case "Blue": button.addStyleName("blue");
+			break;
+			default:
+				int random = (int)(Math.random() * 4);
+				user.setColor(colors[random]);
+				button.addStyleName(colors[random].toLowerCase());
+				break;
+			}
 		}
-    }
-		
+
 	}
-	
+
 	private class MyHandler implements ClickHandler {
 		private User user;
 		private Timestamp in;
 		private Timestamp out;
-		
+
+
 		public MyHandler(User user) {
 			this.user = user;
 		}
-		
+
 		public void onClick(ClickEvent event) {
 			Button clicked = (Button) event.getSource();
-
 			if(clicked.getParent().equals(inPanel)) {
 				clicked.removeFromParent();
 				outPanel.add(clicked);
 				user.setStatus("Out");
 				out = new Timestamp(new Date().getTime());
 				user.setTime(timeDiff(in, out));
+				user.stopTimer();
+
 			}
 			else {
 				clicked.removeFromParent();
 				inPanel.add(clicked);
 				user.setStatus("In");
 				in = new Timestamp (new Date().getTime());
+				user.getName();
+				user.startTimer(clicked,user.getName());
 			}
-						
+
 			sendClickToServer(user);
 		}
-		
+
+
 		private void sendClickToServer(User user) {
 			service.buttonClick(user, new AsyncCallback<Void>() {
 				@Override
@@ -174,7 +210,7 @@ public class AbamathCheckinClient implements AbamathClient {
 				}
 			});
 		}
-		
+
 		private String timeDiff(Timestamp in, Timestamp out) {
 			long diff = out.getTime() - in.getTime();
 			long currentTime = Long.parseLong(user.getTime()) * 60000;
--- src/com/abamath/checkin/shared/User.java
+++ src/com/abamath/checkin/shared/User.java
@@ -2,16 +2,21 @@ package com.abamath.checkin.shared;
 
 import java.io.Serializable;
 
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Button;
+
 public class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	private String name;
 	private String color;
 	private String status;
 	private String time;
-	
+	private boolean timerStatus;
+	//private Timer timer;
+
 	public User(){}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -36,5 +41,76 @@ public class User implements Serializable {
 	public String getTime() {
 		return time;
 	}
+	public void startTimer(Button button, String n)
+	{
+		final String name = n;
+		timerStatus = true;
+		final Button b = button;
+		Timer timer = new Timer() 
+		{
 
+			int seconds = 0; 
+			int minutes = 0;
+			int hours = 0;
+			String timerTime;
+			String minutesString;
+			String secondsString;
+			String hoursString;
+
+			@Override
+			public void run() {
+				if (timerStatus)
+				{
+					if (seconds == 60)
+					{
+						minutes = minutes +1;
+						seconds = 0;
+
+					}
+					else if (minutes == 60)
+					{
+						hours = hours +1;
+						minutes = 0;
+
+					}
+					else if(hours == 24)
+					{
+						hours = 0;
+					}
+					hoursString = hours + "";
+					minutesString = minutes +"";
+					secondsString = seconds + "";
+					if( hours <10)
+					{
+						hoursString = "0" + hours;
+					}
+					if(minutes <10)
+					{
+						minutesString = "0" + minutes;
+					}
+					if(seconds <10)
+					{
+						secondsString = "0" + seconds;
+					}
+
+					seconds++;
+
+					timerTime = hoursString + " : " + minutesString + " : "+  secondsString + " Hours";
+					b.setHTML("<namelabel>" + name + "</namelabel><br/>" + timerTime );
+				}
+				else
+				{
+					seconds = 0; 
+					minutes = 0;
+					hours = 0;
+					b.setHTML ("<namelabel>" + name + "</namelabel><br/>" + ((Integer.parseInt(time)/60)) + " : " +(Integer.parseInt(time)%60) + " : " + "0"+ " Hours");
+				}
+			}
+		};
+		timer.scheduleRepeating(1000);
+	}
+	public void stopTimer()
+	{
+		timerStatus = false;
+	}
 }
